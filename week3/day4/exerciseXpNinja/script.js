cbb38cca45b77e130058f04de047c4b79fab4757
OllyKu <oly.kuskova@gmail.com>
2022-06-30 17:43:12 +0300

    adding daily challenge,xpgold and xpninja

---
+++ week3/day4/exerciseXpNinja/script.js
@@ -0,0 +1,35 @@
+// Exercise 2 : Zip Codes
+// Instruction
+// Harder exercise
+// Hint : This exercise has 2 parts. First, do this exercise without Regular Expressions, then do it using Regular Expressions
+
+// While working in a Post Office you must have the clients’ zip code in order to send them their letters.
+// Ask the client for their zip code and console.log “success” or “error” based on the following rules.
+// Zip codes consists of 5 numbers
+// Must only contain numbers
+// Must not contain any whitespace (spaces)
+// Must not be greater than 5 digits in length
+
+let zipCode = Number(prompt("Enter your zip code: "));
+if ( zipCode.length === 5){
+        console.log("Success");
+}else{
+    console.log("Fail");
+}
+
+// Exercise 3 : Secret Word
+// Instruction
+// Harder exercise
+// Hint : Use Regular Expressions
+
+// Prompt the user for a word and save it to a variable.
+// Delete all the vowels of the word and console.log the result.
+// Bonus: Replace the vowels with another character and console.log the result
+// a = 1
+// e = 2
+// i = 3
+// o = 4
+// u = 5
+ let word = prompt("Enter the word: ");
+ let result = word.replace(/[aeiou]/gi, "");
+ console.log(result);
