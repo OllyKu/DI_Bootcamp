fd23544e613f5ef2d76903f164cccc31e86700cc
OllyKu <oly.kuskova@gmail.com>
2022-08-20 15:25:35 +0300

    week 9 day 2 xp exercises

--- week9/day1/class1.js
+++ week9/day1/class1.js
@@ -12,6 +12,21 @@ const flip = () =>{
 console.log(flip())
 
 // promise example
+/* 
+let promise = new Promise((resolve, reject) => { 
+  // Code to perform the promised task
+  let task_performed = true;
+  if(task_performed) { 
+    resolve('The promised task was performed successfully.'); 
+  } else { 
+    reject('The promised task was not performed.'); 
+  } 
+});  
+promise.then((fromRes) => console.log(fromRes)). 
+  catch((fromRej) => console.log(fromRej));
+
+
+*/
 // new Promise((resolve, reject) =>{
 
 // })
