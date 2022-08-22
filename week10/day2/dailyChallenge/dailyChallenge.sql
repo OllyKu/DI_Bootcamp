5649d0fa28c6dc3868ce3be3e95373958373b147
OllyKu <oly.kuskova@gmail.com>
2022-08-22 21:52:13 +0300

    week 10 day 2 daily

---
+++ week10/day2/dailyChallenge/dailyChallenge.sql
@@ -0,0 +1,5 @@
+-- In this exercise we will be using the actors table from todays lesson.
+-- 1. Count how many actors are in the table. answer 200
+-- 2. Try to add a new actor with some blank fields. What do you think the outcome will be ?
+--SELECT COUNT(1) FROM actor
+--INSERT INTO actor(actor_id, first_name, last_name, last_update) VALUE( , '', '',) error
